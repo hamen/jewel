2d8ac7bc4261c4042e9d2ed54e15e8433e65b40a
Ivan Morgillo <imorgillo@gmail.com>
2024-09-11 17:43:37 +0200

    add more ComboBox to test overlay and text
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- samples/playground/src/main/kotlin/com/ivanmorgillo/jewel/playground/Main.kt
+++ samples/playground/src/main/kotlin/com/ivanmorgillo/jewel/playground/Main.kt
@@ -1,9 +1,10 @@
 package com.ivanmorgillo.jewel.playground
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.getValue
@@ -12,7 +13,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -29,7 +29,9 @@ import org.jetbrains.jewel.intui.standalone.theme.default
 import org.jetbrains.jewel.intui.standalone.theme.lightThemeDefinition
 import org.jetbrains.jewel.intui.window.decoratedWindow
 import org.jetbrains.jewel.ui.ComponentStyling
+import org.jetbrains.jewel.ui.Outline
 import org.jetbrains.jewel.ui.component.ComboBox
+import org.jetbrains.jewel.ui.component.Dropdown
 import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.separator
 import org.jetbrains.jewel.window.DecoratedWindow
@@ -49,14 +51,24 @@ fun main() {
                 state =
                     rememberWindowState(size = DpSize(1000.dp, 600.dp), position = WindowPosition(Alignment.Center)),
                 content = {
-                    val items = remember { listOf("Light", "Dark", "High Contrast", "Darcula", "IntelliJ Light") }
+                    val items = remember {
+                        listOf(
+                            "Cat",
+                            "Elephant",
+                            "Sun",
+                            "Book",
+                            "Laughter",
+                            "Whisper",
+                            "Ocean",
+                            "Serendipity lorem ipsum",
+                            "Umbrella",
+                            "Joy",
+                        )
+                    }
                     var selected: String? by remember { mutableStateOf(items.first()) }
                     val inputTextFieldState = rememberTextFieldState(items.first())
 
-                    Column(
-                        Modifier.background(Color.LightGray).fillMaxSize().padding(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
+                    Column(Modifier.fillMaxSize().padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
                         Text(text = "Selected item: $selected")
                         Text(text = "Input text: ${inputTextFieldState.text}")
                         Text(text = "ComboBox")
@@ -80,6 +92,72 @@ fun main() {
                                 }
                             },
                         )
+
+                        Spacer(Modifier.height(8.dp))
+                        Text(text = "ComboBox")
+                        ComboBox(
+                            outline = Outline.Warning,
+                            inputTextFieldState = inputTextFieldState,
+                            menuContent = {
+                                items.forEach {
+                                    if (it == "---") {
+                                        separator()
+                                    } else {
+                                        selectableItem(
+                                            selected = selected == it,
+                                            onClick = {
+                                                selected = it
+                                                inputTextFieldState.edit { replace(0, length, it) }
+                                            },
+                                        ) {
+                                            Text(it)
+                                        }
+                                    }
+                                }
+                            },
+                        )
+
+                        Spacer(Modifier.height(8.dp))
+                        Text(text = "ComboBox")
+                        ComboBox(
+                            outline = Outline.Error,
+                            inputTextFieldState = inputTextFieldState,
+                            menuContent = {
+                                items.forEach {
+                                    if (it == "---") {
+                                        separator()
+                                    } else {
+                                        selectableItem(
+                                            selected = selected == it,
+                                            onClick = {
+                                                selected = it
+                                                inputTextFieldState.edit { replace(0, length, it) }
+                                            },
+                                        ) {
+                                            Text(it)
+                                        }
+                                    }
+                                }
+                            },
+                        )
+
+                        Spacer(Modifier.height(8.dp))
+                        Text(text = "Dropdown")
+                        Dropdown(
+                            menuContent = {
+                                items.forEach {
+                                    if (it == "---") {
+                                        separator()
+                                    } else {
+                                        selectableItem(selected = selected == it, onClick = { selected = it }) {
+                                            Text(it)
+                                        }
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(selected ?: "---")
+                        }
                     }
                 },
             )
